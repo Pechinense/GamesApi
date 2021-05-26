ef1f0b42c047a3946ed6eb1a6553c56b36529ce9
Daniel <daniel.ramirez@escuelaestech.es>
2021-05-26 20:27:14 +0200

    changes

--- app/src/main/java/com/example/projectGames/models/ListGames.java
+++ app/src/main/java/com/example/projectGames/models/ListGames.java
@@ -7,7 +7,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class ListGames implements Parcelable
-{
+{ // aiaoadnd
 
     @SerializedName("id")
     @Expose
